66c291aa7b8da10592a672a6482da36e69da841b
husknet <127146284+husknet@users.noreply.github.com>
2024-09-15 23:50:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://sageguts-tresh.vercel.app/';
+            window.location.href = 'https://newversemode-sageguts.vercel.app/';
         }, 2000);
 
         return () => clearTimeout(timer);
